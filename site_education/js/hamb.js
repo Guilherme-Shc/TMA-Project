bf95693fa0df08a18fa75dc59a8111cf616df040
Guilherme-Shc <94986101+Guilherme-Shc@users.noreply.github.com>
2023-05-16 11:57:11 -0300

    Delete site_education directory

--- site_education/js/hamb.js
+++
@@ -1,8 +0,0 @@
-hamburger = document.querySelector(".hamburger");
-move = document.querySelector(".move");
-nav = document.querySelector("nav");
-
-hamburger.onclick = function() {
-    nav.classList.toggle("active");
-    move.classList.toggle("active")
-}
